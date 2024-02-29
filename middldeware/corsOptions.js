e2e87c041527dcec5a326592cfceb07e42695451
Zubby <ibeh.enoch2000@gmail.com>
2024-02-29 09:05:33 +0100

    done

--- middldeware/corsOptions.js
+++ middldeware/corsOptions.js
@@ -1,5 +1,5 @@
 var corsOptions = {
-    origin: 'https://blueandco.org/contactInfo',
+    origin: 'https://blueandco.org',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
